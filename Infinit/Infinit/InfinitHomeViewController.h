a53e6fd158670ac54bb6608c8391d2e322874102
IchabodDee <michael.dee33@gmail.com>
2014-12-19 00:53:58 -0500

    Gaetan fixes :-)

---
+++ Infinit/Infinit/InfinitHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfinitHomeViewController.h
+//  Infinit
+//
+//  Created by Michael Dee on 12/18/14.
+//  Copyright (c) 2014 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitHomeViewController : UIViewController
+
+@end
