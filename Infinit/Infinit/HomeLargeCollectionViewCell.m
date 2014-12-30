39cd1d6d6a1ad59c11e6e1a3be9071ad2cd37204
IchabodDee <michael.dee33@gmail.com>
2014-12-30 16:46:17 -0500

    Blur

--- Infinit/Infinit/HomeLargeCollectionViewCell.m
+++ Infinit/Infinit/HomeLargeCollectionViewCell.m
@@ -16,6 +16,22 @@
   
   _imageView.layer.cornerRadius = _imageView.frame.size.height/2;
   
+  _blurImageView.image = [self blurWithCoreImage:_blurImageView.image];
+  
+}
+
+- (UIImage *)blurWithCoreImage:(UIImage *)sourceImage
+{
+  
+  CIImage *imageToBlur = [CIImage imageWithCGImage:sourceImage.CGImage];
+  CIFilter *gaussianBlurFilter = [CIFilter filterWithName: @"CIGaussianBlur"];
+  [gaussianBlurFilter setValue:imageToBlur forKey: @"inputImage"];
+  [gaussianBlurFilter setValue:[NSNumber numberWithFloat: 10] forKey: @"inputRadius"];
+  CIImage *resultImage = [gaussianBlurFilter valueForKey: @"outputImage"];
+  UIImage *endImage = [[UIImage alloc] initWithCIImage:resultImage];
+  
+  return endImage;
+  
 }
 
 @end
