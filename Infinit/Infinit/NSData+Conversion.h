ccaed4e232db9a0f6926ac4071e62ed460250964
Christopher Crone <christopher.crone@infinit.io>
2015-01-07 18:34:22 +0100

    Utilities: Add conversion from NSData to hex.

---
+++ Infinit/Infinit/NSData+Conversion.h
@@ -0,0 +1,15 @@
+//
+//  NSData+Conversion.h
+//  Infinit
+//
+//  Created by Christopher Crone on 07/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData(NSData_Conversion)
+
+- (NSString*)hexadecimalString;
+
+@end
---
+++ Infinit/Infinit/NSData+Conversion.m
@@ -0,0 +1,26 @@
+//
+//  NSData+Conversion.m
+//  Infinit
+//
+//  Created by Christopher Crone on 07/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "NSData+Conversion.h"
+
+@implementation NSData(NSData_Conversion)
+
+- (NSString*)hexadecimalString
+{
+  const unsigned char* buffer = (const unsigned char*)self.bytes;
+  if (!buffer)
+    return @"";
+
+  NSMutableString* res = [[NSMutableString alloc] init];
+  for (int i = 0; i < self.length; ++i)
+    [res appendString:[NSString stringWithFormat:@"%02lx", (unsigned long)buffer[i]]];
+
+  return [NSString stringWithString:res];
+}
+
+@end
