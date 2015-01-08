0c4763dfe31637784ca305e66c2b6dbc67a6cc10
Christopher Crone <christopher.crone@infinit.io>
2015-01-08 20:05:35 +0100

    LoginView: Update.

--- Infinit/Infinit/WelcomeLoginFormView.h
+++ Infinit/Infinit/WelcomeLoginFormView.h
@@ -9,14 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface WelcomeLoginFormView : UIView
-@property (weak, nonatomic) IBOutlet UIButton *back_button;
-@property (weak, nonatomic) IBOutlet UIButton *avatar_button;
-@property (weak, nonatomic) IBOutlet UITextField *login_email_textfield;
-@property (weak, nonatomic) IBOutlet UITextField *login_password_textfield;
-@property (weak, nonatomic) IBOutlet UIButton *login_facebook_button;
-@property (weak, nonatomic) IBOutlet UIImageView *login_email_imageview;
-@property (weak, nonatomic) IBOutlet UIImageView *login_password_imageview;
-@property (weak, nonatomic) IBOutlet UIButton *next_button;
-@property (weak, nonatomic) IBOutlet UILabel *login_error_label;
+
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView* activity;
+@property (weak, nonatomic) IBOutlet UIButton* back_button;
+@property (weak, nonatomic) IBOutlet UIButton* avatar_button;
+@property (weak, nonatomic) IBOutlet UITextField* email_field;
+@property (weak, nonatomic) IBOutlet UITextField* password_field;
+@property (weak, nonatomic) IBOutlet UIButton* facebook_button;
+@property (weak, nonatomic) IBOutlet UIImageView* email_image;
+@property (weak, nonatomic) IBOutlet UIImageView* password_image;
+@property (weak, nonatomic) IBOutlet UIButton* next_button;
+@property (weak, nonatomic) IBOutlet UILabel* error_label;
+
+@property (nonatomic, readonly) CGFloat height;
 
 @end
--- Infinit/Infinit/WelcomeLoginFormView.m
+++ Infinit/Infinit/WelcomeLoginFormView.m
@@ -10,37 +10,15 @@
 
 @implementation WelcomeLoginFormView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-
--(void)awakeFromNib
+- (void)awakeFromNib
 {
-  self.avatar_button.layer.cornerRadius = self.avatar_button.frame.size.height/2;
-  self.avatar_button.layer.borderWidth = 1.0;
-  self.avatar_button.layer.borderColor = [[UIColor colorWithRed:194/255.0 green:211/255.0 blue:211/255.0 alpha:1] CGColor];
+  self.avatar_button.layer.cornerRadius = self.avatar_button.frame.size.height / 2.0f;
   self.avatar_button.clipsToBounds = YES;
-
-  
-  CGFloat spacing = 6.0;
-  CGSize titleSize = [self.avatar_button.titleLabel.text sizeWithAttributes:@{NSFontAttributeName: self.avatar_button.titleLabel.font}];
-  self.avatar_button.imageEdgeInsets = UIEdgeInsetsMake(-(titleSize.height + spacing),
-                                                        0.0,
-                                                        0.0,
-                                                        -titleSize.width);
-  
-  CGSize imageSize = self.avatar_button.imageView.image.size;
-  self.avatar_button.titleEdgeInsets = UIEdgeInsetsMake(0.0,
-                                                        -imageSize.width,
-                                                        -(imageSize.height + spacing),
-                                                        0.0);
-  
 }
 
-
+- (CGFloat)height
+{
+  return 310.0f;
+}
 
 @end
