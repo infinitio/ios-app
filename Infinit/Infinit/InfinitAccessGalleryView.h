0f2baba1731dc23f032f45e8aab6b636926f4e0e
Christopher Crone <christopher.crone@infinit.io>
2015-01-10 15:47:36 +0100

    AccessGalleryView: Add view shown when accessing the gallery the first time.

---
+++ Infinit/Infinit/InfinitAccessGalleryView.h
@@ -0,0 +1,17 @@
+//
+//  InfinitAccessGalleryView.h
+//  Infinit
+//
+//  Created by Christopher Crone on 10/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitAccessGalleryView : UIView
+
+@property (weak, nonatomic) IBOutlet UIButton* access_button;
+@property (weak, nonatomic) IBOutlet UILabel* message_label;
+@property (weak, nonatomic) IBOutlet UIImageView* image_view;
+
+@end
---
+++ Infinit/Infinit/InfinitAccessGalleryView.m
@@ -0,0 +1,18 @@
+//
+//  InfinitAccessGalleryView.m
+//  Infinit
+//
+//  Created by Christopher Crone on 10/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitAccessGalleryView.h"
+
+@implementation InfinitAccessGalleryView
+
+- (void)awakeFromNib
+{
+  self.access_button.layer.cornerRadius = 3.0f;
+}
+
+@end
