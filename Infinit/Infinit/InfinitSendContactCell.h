5dc4258198ac0b9b21c88c8afd55989d2ab81e0a
Christopher Crone <christopher.crone@infinit.io>
2015-01-12 19:31:01 +0100

    SendContactCell: Cell for users not on Infinit.

---
+++ Infinit/Infinit/InfinitSendContactCell.h
@@ -0,0 +1,13 @@
+//
+//  InfinitSendContactCell.h
+//  Infinit
+//
+//  Created by Christopher Crone on 12/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitSendContactCell : UITableViewCell
+
+@end
---
+++ Infinit/Infinit/InfinitSendContactCell.m
@@ -0,0 +1,23 @@
+//
+//  InfinitSendContactCell.m
+//  Infinit
+//
+//  Created by Christopher Crone on 12/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitSendContactCell.h"
+
+@implementation InfinitSendContactCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
