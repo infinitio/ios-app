13219ba669bb1ef33aca4ce248ebb0731d3d01ec
Christopher Crone <christopher.crone@infinit.io>
2015-01-14 15:09:04 +0100

    Add logging in controller.

---
+++ Infinit/Infinit/InfinitLoggingInController.h
@@ -0,0 +1,13 @@
+//
+//  InfinitLoggingInController.h
+//  Infinit
+//
+//  Created by Christopher Crone on 14/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitLoggingInController : UIViewController
+
+@end
---
+++ Infinit/Infinit/InfinitLoggingInController.m
@@ -0,0 +1,17 @@
+//
+//  InfinitLoggingInController.m
+//  Infinit
+//
+//  Created by Christopher Crone on 14/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitLoggingInController.h"
+
+@interface InfinitLoggingInController ()
+
+@end
+
+@implementation InfinitLoggingInController
+
+@end
