48d4e7f834ea35ed6b427a4762ee197e38d87e19
Christopher Crone <christopher.crone@infinit.io>
2015-01-14 17:22:52 +0100

    ALAsset: Add date property.

---
+++ Infinit/Infinit/ALAsset+Date.h
@@ -0,0 +1,15 @@
+//
+//  ALAsset+Date.h
+//  Infinit
+//
+//  Created by Christopher Crone on 14/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@interface ALAsset (Date)
+
+@property (nonatomic) NSDate* date;
+
+@end
---
+++ Infinit/Infinit/ALAsset+Date.m
@@ -0,0 +1,18 @@
+//
+//  ALAsset+Date.m
+//  Infinit
+//
+//  Created by Christopher Crone on 14/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "ALAsset+Date.h"
+
+@implementation ALAsset (Date)
+
+- (NSDate*) date
+{
+  return [self valueForProperty:ALAssetPropertyDate];
+}
+
+@end
