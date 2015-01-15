374090fe3c9803b66c0d0c0b4433b2544a367045
Christopher Crone <christopher.crone@infinit.io>
2015-01-15 20:00:06 +0100

    Add helper for checking if an NSString is an email.

---
+++ Infinit/Infinit/NSString+email.h
@@ -0,0 +1,15 @@
+//
+//  NSString+email.h
+//  Gap
+//
+//  Created by Christopher Crone on 15/01/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (email)
+
+@property (nonatomic, readonly) BOOL isEmail;
+
+@end
