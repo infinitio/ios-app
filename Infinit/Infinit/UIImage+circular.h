7aaa1c88a0ae986b1a50c3696cb4f96231c832f9
Christopher Crone <christopher.crone@infinit.io>
2015-01-16 16:19:43 +0100

    Utilities: Add cicular mask for UIImage.

---
+++ Infinit/Infinit/UIImage+circular.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+circular.h
+//  Infinit
+//
+//  Created by Christopher Crone on 16/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (circular)
+
+- (UIImage*)circularMask;
+
+@end
