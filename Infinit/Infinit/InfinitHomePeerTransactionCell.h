e4b9d693445d78f1a69b3582b1899b3a732d5a57
Christopher Crone <christopher.crone@infinit.io>
2015-01-19 21:08:54 +0100

    HomePeerTransactionCell: Add blurred avatar as background. Add cancel button.

--- Infinit/Infinit/InfinitHomePeerTransactionCell.h
+++ Infinit/Infinit/InfinitHomePeerTransactionCell.h
@@ -12,19 +12,31 @@
 
 #import <Gap/InfinitPeerTransaction.h>
 
+@protocol InfinitHomePeerTransactionCellProtocol;
+
 @interface InfinitHomePeerTransactionCell : UICollectionViewCell
 
-@property (nonatomic, weak) IBOutlet UIImageView* background_view;
 @property (nonatomic, weak) IBOutlet InfinitHomeAvatarView* avatar_view;
-@property (nonatomic, weak) IBOutlet UILabel* time_label;
-@property (nonatomic, weak) IBOutlet UILabel* size_label;
+@property (nonatomic, weak) IBOutlet UIImageView* background_view;
+@property (nonatomic, weak) IBOutlet UIButton* cancel_button;
 @property (nonatomic, weak) IBOutlet UILabel* info_label;
-@property (nonatomic, weak) InfinitPeerTransaction* transaction;
+@property (nonatomic, weak) IBOutlet UILabel* size_label;
+@property (nonatomic, weak) IBOutlet UILabel* time_label;
 
-- (void)setUpWithTransaction:(InfinitPeerTransaction*)transaction;
+@property (nonatomic, weak) InfinitPeerTransaction* transaction;
+@property (nonatomic, readwrite) BOOL cancel_shown;
+
+- (void)setUpWithDelegate:(id<InfinitHomePeerTransactionCellProtocol>)delegate
+              transaction:(InfinitPeerTransaction*)transaction;
 
 - (void)updateAvatar;
 - (void)updateProgressOverDuration:(NSTimeInterval)duration;
 - (void)updateTimeString;
+
+@end
+
+@protocol InfinitHomePeerTransactionCellProtocol <NSObject>
+
+- (void)cellHadCancelTappedForTransaction:(InfinitTransaction*)transaction;
 
 @end
--- Infinit/Infinit/InfinitHomePeerTransactionCell.m
+++ Infinit/Infinit/InfinitHomePeerTransactionCell.m
@@ -13,8 +13,17 @@
 #import <Gap/InfinitTime.h>
 #import <Gap/InfinitDataSize.h>
 
+#import "UIImage+ImageEffects.h"
+
 static NSDictionary* _norm_attrs = nil;
 static NSDictionary* _bold_attrs = nil;
+static UIImage* _mask_image = nil;
+
+@interface InfinitHomePeerTransactionCell ()
+
+@property (nonatomic, readonly) id<InfinitHomePeerTransactionCellProtocol> delegate;
+
+@end
 
 @implementation InfinitHomePeerTransactionCell
 
@@ -22,10 +31,21 @@ static NSDictionary* _bold_attrs = nil;
 {
   [super prepareForReuse];
   self.avatar_view.enable_progress = NO;
+  [self setCancelShown:NO withAnimation:NO];
+  _delegate = nil;
 }
 
 - (void)awakeFromNib
 {
+  if ([[NSProcessInfo processInfo] respondsToSelector:@selector(isOperatingSystemAtLeastVersion:)])
+  {
+    self.background_view.layer.cornerRadius = 3.0f;
+    self.background_view.layer.masksToBounds = YES;
+    UIBlurEffect* blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    UIVisualEffectView* blur_view = [[UIVisualEffectView alloc] initWithEffect:blur];
+    blur_view.frame = self.bounds;
+    [self.background_view addSubview:blur_view];
+  }
   self.layer.masksToBounds = NO;
   self.layer.shadowOpacity = 0.75f;
   self.layer.shadowColor = [InfinitColor colorWithGray:0 alpha:0.5f].CGColor;
@@ -40,13 +60,24 @@ static NSDictionary* _bold_attrs = nil;
     _bold_attrs = @{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Bold" size:15.0f],
                     NSForegroundColorAttributeName: [UIColor whiteColor]};
   }
+  UITapGestureRecognizer* tap =
+    [[UITapGestureRecognizer alloc] initWithTarget:self
+                                            action:@selector(avatarTapped:)];
+  [self.avatar_view addGestureRecognizer:tap];
+  self.cancel_button.center = self.avatar_view.center;
 }
 
-- (void)setUpWithTransaction:(InfinitPeerTransaction*)transaction
+- (void)setUpWithDelegate:(id<InfinitHomePeerTransactionCellProtocol>)delegate
+              transaction:(InfinitPeerTransaction*)transaction
 {
+  _delegate = delegate;
+  InfinitPeerTransaction* old_transaction = _transaction;
   _transaction = transaction;
-  [self setBackgroundImage];
-  [self updateAvatar];
+  if (![old_transaction.other_user isEqual:transaction.other_user])
+  {
+    [self setBackgroundImage];
+    [self updateAvatar];
+  }
   [self updateTimeString];
   [self setInfoString];
   [self setProgress];
@@ -68,27 +99,41 @@ static NSDictionary* _bold_attrs = nil;
 
 - (void)setBackgroundImage
 {
-  CGRect image_rect = self.background_view.bounds;
-  UIGraphicsBeginImageContextWithOptions(image_rect.size, NO, 0.0f);
-  UIBezierPath* rounded_corners = [UIBezierPath bezierPathWithRoundedRect:image_rect
-                                                             cornerRadius:3.0f];
-  [rounded_corners addClip];
-  [[InfinitColor colorWithGray:41 alpha:0.8f] set];
-  UIRectFill(image_rect);
-  UIImage* background_image = UIGraphicsGetImageFromCurrentImageContext();
-  UIGraphicsEndImageContext();
-  self.background_view.image = background_image;
+  if (![[NSProcessInfo processInfo] respondsToSelector:@selector(isOperatingSystemAtLeastVersion:)])
+  {
+      CGRect image_rect = self.background_view.bounds;
+      UIGraphicsBeginImageContextWithOptions(image_rect.size, NO, 0.0f);
+      [[UIColor blackColor] set];
+      [[UIBezierPath bezierPathWithRoundedRect:image_rect cornerRadius:3.0f] addClip];
+      [[self.transaction.other_user.avatar applyDarkEffect] drawInRect:image_rect];
+      UIImage* background_image = UIGraphicsGetImageFromCurrentImageContext();
+      UIGraphicsEndImageContext();
+      self.background_view.image = background_image;
+  }
+  else
+  {
+    self.background_view.image = self.transaction.other_user.avatar;
+  }
 }
 
 - (void)setInfoString
 {
   NSString* res = nil;
   NSRange bold_range = NSMakeRange(0, 0);
-  NSString* file_count = [NSString stringWithFormat:NSLocalizedString(@"%@ files", nil),
-                          @(self.transaction.files.count)];
+  NSString* file_count;
+  if (self.transaction.files.count == 1)
+  {
+    file_count = [NSString stringWithFormat:NSLocalizedString(@"%@ file", nil),
+                  @(self.transaction.files.count)];
+  }
+  else
+  {
+    file_count = [NSString stringWithFormat:NSLocalizedString(@"%@ files", nil),
+                  @(self.transaction.files.count)];
+  }
   NSString* other_name = self.transaction.other_user.fullname;
   if (self.transaction.other_user.is_self)
-    other_name = NSLocalizedString(@"me", nil);
+    other_name = NSLocalizedString(@"yourself", nil);
   switch (self.transaction.status)
   {
     case gap_transaction_new:
@@ -108,8 +153,7 @@ static NSDictionary* _bold_attrs = nil;
       break;
     }
     case gap_transaction_on_other_device:
-      res = [NSString stringWithFormat:NSLocalizedString(@"Transfer of %@ with %@ on another device", nil),
-             file_count, other_name];
+      res = NSLocalizedString(@"Transferring on another device", nil);
       break;
     case gap_transaction_waiting_accept:
       if (self.transaction.from_device)
@@ -128,7 +172,7 @@ static NSDictionary* _bold_attrs = nil;
              other_name];
       break;
     case gap_transaction_cloud_buffered:
-      res = [NSString stringWithFormat:NSLocalizedString(@"Uploaded %@. Waiting for %@ to download", nil),
+      res = [NSString stringWithFormat:NSLocalizedString(@"Uploaded %@, waiting for %@ to download", nil),
              file_count, other_name];
       break;
     case gap_transaction_finished:
@@ -144,16 +188,21 @@ static NSDictionary* _bold_attrs = nil;
       }
       break;
     case gap_transaction_failed:
-      res = [NSString stringWithFormat:NSLocalizedString(@"Transfer of %@ with %@ failed", nil),
-             file_count, other_name];
+      res = NSLocalizedString(@"Transfer stopped, please try again", nil);
       break;
     case gap_transaction_canceled:
-      res = [NSString stringWithFormat:NSLocalizedString(@"Transfer of %@ with %@ canceled", nil),
-             file_count, other_name];
+      res = NSLocalizedString(@"Transfer canceled", nil);
       break;
     case gap_transaction_rejected:
-      res = [NSString stringWithFormat:NSLocalizedString(@"Transfer of %@ with %@ rejected", nil),
-             file_count, other_name];
+      if (self.transaction.other_user.is_self)
+      {
+        res = NSLocalizedString(@"You declined the transfer", nil);
+      }
+      else
+      {
+        res = [NSString stringWithFormat:NSLocalizedString(@"%@ declined the transfer", nil),
+               other_name];
+      }
       break;
     case gap_transaction_paused:
       res = [NSString stringWithFormat:NSLocalizedString(@"Transfer of %@ with %@ paused", nil),
@@ -178,6 +227,7 @@ static NSDictionary* _bold_attrs = nil;
 - (void)updateAvatar
 {
   self.avatar_view.image = self.transaction.other_user.avatar;
+  [self setBackgroundImage];
 }
 
 - (void)updateProgressOverDuration:(NSTimeInterval)duration
@@ -199,6 +249,69 @@ static NSDictionary* _bold_attrs = nil;
       break;
   }
   self.time_label.text = label_text;
+}
+
+#pragma mark - Button Handling
+
+- (void)avatarTapped:(id)sender
+{
+  if (self.transaction.done)
+    return;
+  [self setCancelShown:!self.cancel_shown withAnimation:YES];
+}
+
+- (void)setCancel_shown:(BOOL)cancel_shown
+{
+  [self setCancelShown:cancel_shown withAnimation:YES];
+}
+
+- (void)setCancelShown:(BOOL)shown
+         withAnimation:(BOOL)animate
+{
+  if (shown == _cancel_shown)
+    return;
+  _cancel_shown = shown;
+  self.cancel_button.enabled = _cancel_shown;
+
+  CGAffineTransform transform;
+
+  if (_cancel_shown)
+  {
+    CGFloat dx = (self.avatar_view.center.x + self.cancel_button.bounds.size.width) / 2.0f;
+
+    CGAffineTransform translate = CGAffineTransformMakeTranslation(dx, 0.0f);
+    CGAffineTransform roll = CGAffineTransformMakeRotation(M_PI);
+    transform = CGAffineTransformConcat(translate, roll);
+  }
+  else
+  {
+    transform = CGAffineTransformIdentity;
+  }
+  if (animate)
+  {
+    [UIView animateWithDuration:0.3f
+                          delay:0.0f
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^
+     {
+       self.cancel_button.transform = transform;
+       [self layoutIfNeeded];
+     } completion:^(BOOL finished)
+     {
+       if (!finished)
+         self.cancel_button.transform = transform;
+     }];
+  }
+  else
+  {
+    self.cancel_button.transform = transform;
+  }
+}
+
+- (IBAction)cancelTapped:(id)sender
+{
+  self.cancel_shown = NO;
+  [_delegate cellHadCancelTappedForTransaction:self.transaction];
 }
 
 @end
