313a1e86fac64980953dee66c2bb48335863f6e2
Christopher Crone <christopher.crone@infinit.io>
2015-01-19 21:01:25 +0100

    LoggingInView: Update.

---
+++ Infinit/Infinit/InfinitLoggingInViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfinitLoggingInViewController.h
+//  Infinit
+//
+//  Created by Christopher Crone on 19/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitLoggingInViewController : UIViewController
+
+@end
---
+++ Infinit/Infinit/InfinitLoggingInViewController.m
@@ -0,0 +1,32 @@
+//
+//  InfinitLoggingInViewController.m
+//  Infinit
+//
+//  Created by Christopher Crone on 19/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitLoggingInViewController.h"
+
+@interface InfinitLoggingInViewController ()
+
+@property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activity;
+
+@end
+
+@implementation InfinitLoggingInViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  self.navigationItem.titleView =
+    [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon-logo-red"]];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+  [self.activity stopAnimating];
+  [super viewWillDisappear:animated];
+}
+
+@end
