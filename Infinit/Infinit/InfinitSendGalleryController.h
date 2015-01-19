4594759064b5f9a17b6c74f45bd23e8cd03c7f55
Christopher Crone <christopher.crone@infinit.io>
2015-01-19 21:01:44 +0100

    SendGalleryController: Rename and refactor.

---
+++ Infinit/Infinit/InfinitSendGalleryController.h
@@ -0,0 +1,15 @@
+//
+//  InfinitSendGalleryController.h
+//  Infinit
+//
+//  Created by Michael Dee on 12/17/14.
+//  Copyright (c) 2014 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitSendGalleryController : UICollectionViewController
+
+- (void)resetView;
+
+@end
