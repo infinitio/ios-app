adcfc5c3a79673285380ef7d433059e9d95d68dc
Christopher Crone <christopher.crone@infinit.io>
2015-01-20 15:48:45 +0100

    HomeOnboardingCell: Add cell for onboarding messages.

---
+++ Infinit/Infinit/InfinitHomeOnboardingCell.h
@@ -0,0 +1,17 @@
+//
+//  InfinitHomeOnboardingCell.h
+//  Infinit
+//
+//  Created by Christopher Crone on 20/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "InfinitHomeAbstractCell.h"
+
+@interface InfinitHomeOnboardingCell : InfinitHomeAbstractCell
+
+@property (nonatomic, weak) IBOutlet UILabel* message;
+
+@end
---
+++ Infinit/Infinit/InfinitHomeOnboardingCell.m
@@ -0,0 +1,19 @@
+//
+//  InfinitHomeOnboardingCell.m
+//  Infinit
+//
+//  Created by Christopher Crone on 20/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitHomeOnboardingCell.h"
+
+@implementation InfinitHomeOnboardingCell
+
+- (void)awakeFromNib
+{
+  [super awakeFromNib];
+  self.layer.cornerRadius = 3.0f;
+}
+
+@end
