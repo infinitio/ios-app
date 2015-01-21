26335585ee738a68b1b56e43d594a8752035d57e
Christopher Crone <christopher.crone@infinit.io>
2015-01-21 13:35:15 +0100

    SettingsViewController: Update to include report a problem.

--- Infinit/Infinit/InfinitSettingsViewController.m
+++ Infinit/Infinit/InfinitSettingsViewController.m
@@ -9,22 +9,28 @@
 #import "InfinitSettingsViewController.h"
 
 #import "InfinitColor.h"
+#import "InfinitSettingsUserCell.h"
 #import "InfinitTabBarController.h"
 
 #import <Gap/InfinitStateManager.h>
 #import <Gap/InfinitStateResult.h>
+#import <Gap/InfinitUserManager.h>
 
 typedef NS_ENUM(NSUInteger, InfinitSettingsSections)
 {
   InfinitSettingsSectionAccount = 0,
+  InfinitSettingsSectionFeedback,
+  InfinitSettingsSectionLogout,
 };
 
-typedef NS_ENUM(NSUInteger, InfinitAccountSettings)
+typedef NS_ENUM(NSUInteger, InfinitLogoutSettings)
 {
-  InfinitAccountSettingsLogout = 0,
+  InfinitLogoutSettingLogout = 0,
 };
 
 @interface InfinitSettingsViewController ()
+
+@property (nonatomic, weak) IBOutlet InfinitSettingsUserCell* user_cell;
 
 @end
 
@@ -34,8 +40,6 @@ typedef NS_ENUM(NSUInteger, InfinitAccountSettings)
 {
   self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
   [super viewDidLoad];
-
-  self.navigationController.navigationBar.clipsToBounds = YES;
   NSDictionary* nav_bar_attrs = @{NSFontAttributeName: [UIFont fontWithName:@"SourceSansPro-Bold"
                                                                        size:17.0f],
                                   NSForegroundColorAttributeName: [InfinitColor colorWithRed:81
@@ -44,20 +48,48 @@ typedef NS_ENUM(NSUInteger, InfinitAccountSettings)
   [self.navigationController.navigationBar setTitleTextAttributes:nav_bar_attrs];
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+  [self.user_cell configureWithUser:[[InfinitUserManager sharedInstance] me]];
+  [super viewWillAppear:animated];
+}
+
 #pragma mark - Table View Delegate
 
 - (void)tableView:(UITableView*)tableView
 didSelectRowAtIndexPath:(NSIndexPath*)indexPath
 {
-  if (indexPath.section == InfinitSettingsSectionAccount)
+  if (indexPath.section == InfinitSettingsSectionLogout &&
+      indexPath.row == InfinitLogoutSettingLogout)
   {
-    if (indexPath.row == InfinitAccountSettingsLogout)
-    {
-      [[InfinitStateManager sharedInstance] logoutPerformSelector:@selector(logoutCallback:)
-                                                         onObject:self];
-      [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
-    }
+    [[InfinitStateManager sharedInstance] logoutPerformSelector:@selector(logoutCallback:)
+                                                       onObject:self];
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
   }
+}
+
+- (CGFloat)tableView:(UITableView*)tableView
+heightForHeaderInSection:(NSInteger)section
+{
+  return 1.0f;
+}
+
+- (CGFloat)tableView:(UITableView*)tableView
+heightForFooterInSection:(NSInteger)section
+{
+  return 32.0f;
+}
+
+- (UIView*)tableView:(UITableView*)tableView
+viewForHeaderInSection:(NSInteger)section
+{
+  return [[UIView alloc] initWithFrame:CGRectZero];
+}
+
+- (UIView*)tableView:(UITableView*)tableView
+viewForFooterInSection:(NSInteger)section
+{
+  return [[UIView alloc] initWithFrame:CGRectZero];
 }
 
 #pragma mark - Callbacks
