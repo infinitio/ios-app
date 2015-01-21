ab9de28077fcadde485844d47105fce35cc53390
Christopher Crone <christopher.crone@infinit.io>
2015-01-21 13:24:27 +0100

    SendNavigationBar: Subclass bar to make it bigger.

---
+++ Infinit/Infinit/InfinitSendNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  InfinitSendNavigationBar.h
+//  Infinit
+//
+//  Created by Christopher Crone on 20/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitSendNavigationBar : UINavigationBar
+
+@end
---
+++ Infinit/Infinit/InfinitSendNavigationBar.m
@@ -0,0 +1,18 @@
+//
+//  InfinitSendNavigationBar.m
+//  Infinit
+//
+//  Created by Christopher Crone on 20/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitSendNavigationBar.h"
+
+@implementation InfinitSendNavigationBar
+
+- (CGSize)sizeThatFits:(CGSize)size
+{
+  return CGSizeMake(self.bounds.size.width, 54.0f);
+}
+
+@end
