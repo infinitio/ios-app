cb6dc5bba7381cccc3a73dd18d043426fe140713
Christopher Crone <christopher.crone@infinit.io>
2015-01-23 15:51:41 +0100

    SendUserCell: Set colour of 'other devices'.

--- Infinit/Infinit/InfinitSendUserCell.m
+++ Infinit/Infinit/InfinitSendUserCell.m
@@ -8,7 +8,11 @@
 
 #import "InfinitSendUserCell.h"
 
+#import "InfinitColor.h"
+
 #import "UIImage+circular.h"
+
+static NSDictionary* _me_attrs = nil;
 
 @implementation InfinitSendUserCell
 
@@ -21,6 +25,33 @@
     self.user_type_view.image = [UIImage imageNamed:@"icon-contact-favorite"].circularMask;
   else
     self.user_type_view.image = [UIImage imageNamed:@"icon-contact-infinit"].circularMask;
+  if (self.contact.infinit_user.is_self)
+    [self setSelfTextColor];
+}
+
+- (void)setSelfTextColor
+{
+  if (_me_attrs == nil)
+  {
+    _me_attrs = @{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:17.0f],
+                  NSForegroundColorAttributeName: [InfinitColor colorWithGray:175]};
+  }
+  NSRange color_range =
+    [self.name_label.text rangeOfString:NSLocalizedString(@"(other devices)", nil)];
+  if (color_range.location != NSNotFound)
+  {
+    NSMutableAttributedString* res = [self.name_label.attributedText mutableCopy];
+    [res setAttributes:_me_attrs range:color_range];
+    self.name_label.attributedText = res;
+  }
+}
+
+- (void)setSelected:(BOOL)selected
+           animated:(BOOL)animated
+{
+  [super setSelected:selected animated:animated];
+  if (self.contact.infinit_user.is_self && !selected)
+    [self setSelfTextColor];
 }
 
 - (void)updateAvatar
