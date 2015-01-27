e69c35e3d8ff000f24105e979d181ae80bd2a85f
Christopher Crone <christopher.crone@infinit.io>
2015-01-27 14:50:11 +0100

    PortraitNavigationController: Class to make navigation portrait only.

---
+++ Infinit/Infinit/InfinitPortraitNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  InfinitPortraitNavigationController.h
+//  Infinit
+//
+//  Created by Christopher Crone on 27/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitPortraitNavigationController : UINavigationController
+
+@end
---
+++ Infinit/Infinit/InfinitPortraitNavigationController.m
@@ -0,0 +1,29 @@
+//
+//  InfinitPortraitNavigationController.m
+//  Infinit
+//
+//  Created by Christopher Crone on 27/01/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitPortraitNavigationController.h"
+
+@interface InfinitPortraitNavigationController ()
+
+@end
+
+@implementation InfinitPortraitNavigationController
+
+#pragma mark - Rotation
+
+- (BOOL)shouldAutorotate
+{
+  return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+  return UIInterfaceOrientationMaskPortrait;
+}
+
+@end
