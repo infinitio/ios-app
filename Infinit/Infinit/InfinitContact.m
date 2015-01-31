0c6d8e9e003ac842bdf82b22633de378f94da518
Christopher Crone <christopher.crone@infinit.io>
2015-01-31 18:10:24 +0100

    Contact: Set me name.

--- Infinit/Infinit/InfinitContact.m
+++ Infinit/Infinit/InfinitContact.m
@@ -100,7 +100,7 @@
     _avatar = user.avatar;
     _emails = nil;
     if (user.is_self)
-      _fullname = NSLocalizedString(@"Me (other devices)", nil);
+      _fullname = NSLocalizedString(@"Me", nil);
     else
       _fullname = user.fullname;
     NSArray* temp = [self.fullname componentsSeparatedByString:@" "];
