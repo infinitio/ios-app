8e2ce380bd7fc848e9012d7f60c3d72ae6b46076
Christopher Crone <christopher.crone@infinit.io>
2015-02-06 19:02:01 +0100

    HomeOnboardingCell: Set shadow.

--- Infinit/Infinit/InfinitHomeOnboardingCell.m
+++ Infinit/Infinit/InfinitHomeOnboardingCell.m
@@ -8,12 +8,23 @@
 
 #import "InfinitHomeOnboardingCell.h"
 
+#import "InfinitColor.h"
+#import "InfinitHostDevice.h"
+
 @implementation InfinitHomeOnboardingCell
 
 - (void)awakeFromNib
 {
   [super awakeFromNib];
-  self.layer.cornerRadius = 3.0f;
+  if ([InfinitHostDevice deviceCPU] >= InfinitCPUType_ARM64_v8)
+  {
+    self.layer.cornerRadius = 3.0f;
+    self.layer.masksToBounds = NO;
+    self.layer.shadowOpacity = 0.15f;
+    self.layer.shadowRadius = 2.0f;
+    self.layer.shadowColor = [InfinitColor colorWithGray:0].CGColor;
+    self.layer.shadowOffset = CGSizeMake(0.0f, 1.0f);
+  }
 }
 
 @end
