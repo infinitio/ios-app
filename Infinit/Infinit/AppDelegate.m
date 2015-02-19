77e2be566468a51c0bba58c374317a8efff900c2
Christopher Crone <christopher.crone@infinit.io>
2015-02-19 15:18:50 +0100

    AppDelegate: Remove notifications on open app.

--- Infinit/Infinit/AppDelegate.m
+++ Infinit/Infinit/AppDelegate.m
@@ -166,6 +166,7 @@ didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 - (void)applicationDidBecomeActive:(UIApplication*)application
 {
   // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  [[UIApplication sharedApplication] cancelAllLocalNotifications];
   [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
 }
 
