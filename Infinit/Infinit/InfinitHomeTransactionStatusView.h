3b3a327cb4f25780195a303d63a877697fa95e77
Christopher Crone <christopher.crone@infinit.io>
2015-02-20 21:10:00 +0100

    HomeTransactionStatusView: Add view for handling dynamic transfer icon and other static status icons.

---
+++ Infinit/Infinit/InfinitHomeTransactionStatusView.h
@@ -0,0 +1,15 @@
+//
+//  InfinitHomeTransactionStatusView.h
+//  Infinit
+//
+//  Created by Christopher Crone on 20/02/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitHomeTransactionStatusView : UIImageView
+
+@property (nonatomic, readwrite) BOOL run_transfer_animation;
+
+@end
