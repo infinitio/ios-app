c06810752af3d74fe40c7c3fd2857914a2fe0c16
Christopher Crone <christopher.crone@infinit.io>
2015-02-20 21:16:31 +0100

    FilesNavigationController: Handle opening files/folders from outside.

--- Infinit/Infinit/InfinitFilesNavigationController.h
+++ Infinit/Infinit/InfinitFilesNavigationController.h
@@ -8,6 +8,11 @@
 
 #import "InfinitPortraitNavigationController.h"
 
+#import "InfinitFolderModel.h"
+
 @interface InfinitFilesNavigationController : InfinitPortraitNavigationController
+
+@property (nonatomic, weak, readwrite) InfinitFolderModel* folder;
+@property (nonatomic, readwrite) NSInteger file_index;
 
 @end
