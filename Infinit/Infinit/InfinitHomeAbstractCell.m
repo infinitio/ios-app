cae3ad7694a5d569684ea48944b57457604703a7
Christopher Crone <christopher.crone@infinit.io>
2015-02-20 21:15:17 +0100

    HomeAbstractCell: Shadow needs to be dynamic if we animate size.

--- Infinit/Infinit/InfinitHomeAbstractCell.m
+++ Infinit/Infinit/InfinitHomeAbstractCell.m
@@ -13,13 +13,16 @@
 
 @implementation InfinitHomeAbstractCell
 
-- (void)layoutSubviews
+- (void)awakeFromNib
 {
-  [super layoutSubviews];
   if ([InfinitHostDevice deviceCPU] >= InfinitCPUType_ARM64_v8 && self.layer.shadowPath == nil)
   {
-    self.layer.shadowPath =
-      [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:3.0f].CGPath;
+    self.layer.cornerRadius = 3.0f;
+    self.layer.masksToBounds = NO;
+    self.layer.shadowOpacity = 0.15f;
+    self.layer.shadowRadius = 2.0f;
+    self.layer.shadowColor = [InfinitColor colorWithGray:0].CGColor;
+    self.layer.shadowOffset = CGSizeMake(0.0f, 1.0f);
   }
 }
 
