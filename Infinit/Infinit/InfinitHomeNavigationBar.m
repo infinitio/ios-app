02cdab1418dc288a2f9d18fe347e76277c5d299d
Christopher Crone <christopher.crone@infinit.io>
2015-02-23 18:30:19 +0100

    HomeNavigationBar: Customise home navigation bar.

---
+++ Infinit/Infinit/InfinitHomeNavigationBar.m
@@ -0,0 +1,27 @@
+//
+//  InfinitHomeNavigationBar.m
+//  Infinit
+//
+//  Created by Christopher Crone on 21/02/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitHomeNavigationBar.h"
+
+@implementation InfinitHomeNavigationBar
+
+- (CGSize)sizeThatFits:(CGSize)size
+{
+  if (self.large)
+    return CGSizeMake(self.superview.bounds.size.width, 54.0f);
+  return [super sizeThatFits:size];
+}
+
+- (void)setLarge:(BOOL)large
+{
+  if (self.large == large)
+    return;
+  _large = large;
+}
+
+@end
