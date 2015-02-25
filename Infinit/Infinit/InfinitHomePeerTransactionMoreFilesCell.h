1d937ab9f9c39efdb86db195540670aeb9da9a27
Christopher Crone <christopher.crone@infinit.io>
2015-02-25 17:49:53 +0100

    HomePeerTransactionMoreFilesCell: Add cell for showing more files.

---
+++ Infinit/Infinit/InfinitHomePeerTransactionMoreFilesCell.h
@@ -0,0 +1,15 @@
+//
+//  InfinitHomePeerTransactionMoreFilesCell.h
+//  Infinit
+//
+//  Created by Christopher Crone on 25/02/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitHomePeerTransactionMoreFilesCell : UICollectionViewCell
+
+@property (nonatomic, readwrite) NSUInteger count;
+
+@end
---
+++ Infinit/Infinit/InfinitHomePeerTransactionMoreFilesCell.m
@@ -0,0 +1,47 @@
+//
+//  InfinitHomePeerTransactionMoreFilesCell.m
+//  Infinit
+//
+//  Created by Christopher Crone on 25/02/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitHomePeerTransactionMoreFilesCell.h"
+
+#import "InfinitColor.h"
+
+@interface InfinitHomePeerTransactionMoreFilesCell ()
+
+@property (nonatomic, weak) IBOutlet UIImageView* icon_view;
+@property (nonatomic, weak) IBOutlet UILabel* files_label;
+
+@end
+
+@implementation InfinitHomePeerTransactionMoreFilesCell
+
+static UIImage* _icon_image = nil;
+
+- (void)awakeFromNib
+{
+  if (_icon_image == nil)
+  {
+    UIGraphicsBeginImageContextWithOptions(self.icon_view.bounds.size, NO, 0.0f);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    UIBezierPath* shape = [UIBezierPath bezierPathWithRoundedRect:self.icon_view.bounds
+                                                     cornerRadius:2.0f];
+    CGContextAddPath(context, shape.CGPath);
+    [[InfinitColor colorWithGray:245] set];
+    CGContextFillPath(context);
+    _icon_image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+  }
+  self.icon_view.image = _icon_image;
+}
+
+- (void)setCount:(NSUInteger)count
+{
+  self.icon_view.image = _icon_image;
+  self.files_label.text = [NSString stringWithFormat:@"+%lu", count];
+}
+
+@end
