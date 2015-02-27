7b6c261c7d4d1226b6ef686e7cdb8d6412e153e9
Christopher Crone <christopher.crone@infinit.io>
2015-02-27 15:59:33 +0100

    SendEmailCell: Add cell for entered emails.

---
+++ Infinit/Infinit/InfinitSendEmailCell.h
@@ -0,0 +1,15 @@
+//
+//  InfinitSendEmailCell.h
+//  Infinit
+//
+//  Created by Christopher Crone on 27/02/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitSendEmailCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel* email_label;
+
+@end
---
+++ Infinit/Infinit/InfinitSendEmailCell.m
@@ -0,0 +1,13 @@
+//
+//  InfinitSendEmailCell.m
+//  Infinit
+//
+//  Created by Christopher Crone on 27/02/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitSendEmailCell.h"
+
+@implementation InfinitSendEmailCell
+
+@end
