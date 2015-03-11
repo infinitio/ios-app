d8876f2f9ff00c6e84f4e787365e39d3cf39f281
Christopher Crone <christopher.crone@infinit.io>
2015-03-11 18:04:15 +0100

    SettingsNavigationController: Only pop to root when not editing profile.

--- Infinit/Infinit/InfinitSettingsNavigationController.m
+++ Infinit/Infinit/InfinitSettingsNavigationController.m
@@ -8,7 +8,11 @@
 
 #import "InfinitSettingsNavigationController.h"
 
+#import "InfinitSettingsEditProfileViewController.h"
+
 @interface InfinitSettingsNavigationController ()
+
+@property (nonatomic, readwrite) BOOL editing_profile;
 
 @end
 
@@ -17,7 +21,30 @@
 - (void)viewWillAppear:(BOOL)animated
 {
   [super viewWillAppear:animated];
-  [self popToRootViewControllerAnimated:NO];
+  if (!self.editing_profile)
+    [self popToRootViewControllerAnimated:NO];
+}
+
+- (void)pushViewController:(UIViewController*)viewController
+                  animated:(BOOL)animated
+{
+  if ([viewController isKindOfClass:InfinitSettingsEditProfileViewController.class]) // iOS 7
+  {
+    self.editing_profile = YES;
+  }
+  else if ([viewController isKindOfClass:UINavigationController.class]) // iOS 8
+  {
+    UINavigationController* nav_controller = (UINavigationController*)viewController;
+    if ([nav_controller.topViewController isKindOfClass:InfinitSettingsEditProfileViewController.class])
+      self.editing_profile = YES;
+  }
+  [super pushViewController:viewController animated:animated];
+}
+
+- (UIViewController*)popViewControllerAnimated:(BOOL)animated
+{
+  self.editing_profile = NO;
+  return [super popViewControllerAnimated:animated];
 }
 
 @end
