4d24a9b9f7afa36e1116ffb7d5f2dbe8741632a9
Christopher Crone <christopher.crone@infinit.io>
2015-04-03 16:00:12 +0200

    Constants: Add fil containing shared constants.

---
+++ Infinit/Infinit/InfinitConstants.h
@@ -0,0 +1,14 @@
+//
+//  InfinitConstants.h
+//  Infinit
+//
+//  Created by Christopher Crone on 03/04/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#ifndef Infinit_InfinitConstants_h
+#define Infinit_InfinitConstants_h
+
+#define kInfinitAlbumName @"Infinit"
+
+#endif
