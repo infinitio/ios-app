7b404f1c83fd302debaba9ba952f3b62d49749f3
Christopher Crone <christopher.crone@infinit.io>
2015-04-08 19:35:22 +0200

    Constants: Add application ID. Add group name. Add rate links.

--- Infinit/Infinit/InfinitConstants.h
+++ Infinit/Infinit/InfinitConstants.h
@@ -9,6 +9,13 @@
 #ifndef Infinit_InfinitConstants_h
 #define Infinit_InfinitConstants_h
 
+#define kInfinitStoreRatingLinkiOS8 @"https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software"
+#define kInfinitStoreRatingLink @"https://itunes.apple.com/app/apple-store/idAPP_ID"
+
+#define kInfinitAppStoreId @"955849852"
+
 #define kInfinitAlbumName @"Infinit"
+
+#define kInfinitAppGroupName @"group.io.infinit.InfinitMobile"
 
 #endif
