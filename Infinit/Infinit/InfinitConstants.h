65de3a10b2461f9b04554cbdedd5751b9e013d31
Christopher Crone <christopher.crone@infinit.io>
2015-04-09 16:34:10 +0200

    Constants: Add comments.

--- Infinit/Infinit/InfinitConstants.h
+++ Infinit/Infinit/InfinitConstants.h
@@ -9,13 +9,17 @@
 #ifndef Infinit_InfinitConstants_h
 #define Infinit_InfinitConstants_h
 
+/// URLs used for rating from the application.
 #define kInfinitStoreRatingLinkiOS8 @"https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software"
 #define kInfinitStoreRatingLink @"https://itunes.apple.com/app/apple-store/idAPP_ID"
 
+/// App Store ID.
 #define kInfinitAppStoreId @"955849852"
 
+/// Photos gallery name.
 #define kInfinitAlbumName @"Infinit"
 
+/// Application group name.
 #define kInfinitAppGroupName @"group.io.infinit.InfinitMobile"
 
 #endif
