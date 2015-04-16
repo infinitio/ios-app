1b910188f24c6a7583a0dadb6c1094f09eb3377d
Christopher Crone <christopher.crone@infinit.io>
2015-04-16 17:19:24 +0200

    TabBarController-iPad: Add tab bar controller for iPad.

---
+++ Infinit/Infinit/InfinitTabBarController_iPad.h
@@ -0,0 +1,13 @@
+//
+//  InfinitTabBarController_iPad.h
+//  Infinit
+//
+//  Created by Christopher Crone on 15/04/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitTabBarController_iPad : UITabBarController
+
+@end
---
+++ Infinit/Infinit/InfinitTabBarController_iPad.m
@@ -0,0 +1,109 @@
+//
+//  InfinitTabBarController_iPad.m
+//  Infinit
+//
+//  Created by Christopher Crone on 15/04/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitTabBarController_iPad.h"
+
+#import "InfinitColor.h"
+
+typedef NS_ENUM(NSUInteger, InfinitTabBarIndex)
+{
+  InfinitTabBarIndexHome = 0,
+  InfinitTabBarIndexSettings,
+};
+
+@interface InfinitTabBarController_iPad () <UITabBarControllerDelegate>
+
+@property (nonatomic, readonly) UIView* selection_indicator;
+
+@end
+
+@implementation InfinitTabBarController_iPad
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  [self.storyboard instantiateViewControllerWithIdentifier:@"home_nav_controller"];
+  self.delegate = self;
+  for (NSUInteger index = 0; index < self.tabBar.items.count; index++)
+  {
+    [self.tabBar.items[index] setImageInsets:UIEdgeInsetsMake(5.0f, 0.0f, -5.0f, 0.0f)];
+    [self.tabBar.items[index] setImage:[self imageForTabBarItem:index selected:NO]];
+    [self.tabBar.items[index] setSelectedImage:[self imageForTabBarItem:index selected:YES]];
+  }
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+  [super viewDidAppear:animated];
+  CGFloat width = self.tabBar.frame.size.width;
+  [[UITabBar appearance] setBackgroundImage:[[UIImage alloc] init]];
+  self.tabBar.shadowImage = [[UIImage alloc] init];
+  UIView* shadow_line = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, width, 1.0f)];
+  shadow_line.backgroundColor = [InfinitColor colorWithGray:216.0f];
+  [self.tabBar addSubview:shadow_line];
+  NSInteger count = self.viewControllers.count;
+  _selection_indicator =
+    [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, width / count, 1.0f)];
+  self.selection_indicator.backgroundColor =
+    [InfinitColor colorFromPalette:InfinitPaletteColorBurntSienna];
+  [self.tabBar addSubview:self.selection_indicator];
+}
+
+#pragma mark - UITabBarController
+
+- (void)setSelectedIndex:(NSUInteger)selectedIndex
+{
+  if (selectedIndex == self.selectedIndex)
+    return;
+  [super setSelectedIndex:selectedIndex];
+  [self selectorToPosition:selectedIndex];
+}
+
+- (BOOL)tabBarController:(UITabBarController*)tabBarController
+shouldSelectViewController:(UIViewController*)viewController
+{
+  [self selectorToPosition:[self.viewControllers indexOfObject:viewController]];
+  return YES;
+}
+
+#pragma mark - Tab Selector
+
+- (void)selectorToPosition:(NSInteger)position
+{
+  NSUInteger count = self.viewControllers.count;
+  CGFloat width = self.tabBar.frame.size.width;
+  self.selection_indicator.frame = CGRectMake(position == 0 ? 0.0f : width / (count * position),
+                                              0.0f,
+                                              width / count,
+                                              1.0f);
+}
+
+#pragma mark - Helpers
+
+- (UIImage*)imageForTabBarItem:(InfinitTabBarIndex)index
+                      selected:(BOOL)selected
+{
+  NSString* image_name = nil;
+  switch (index)
+  {
+    case InfinitTabBarIndexHome:
+      image_name = @"icon-tab-home";
+      break;
+    case InfinitTabBarIndexSettings:
+      image_name = @"icon-tab-settings";
+      break;
+
+    default:
+      return nil;
+  }
+  if (selected)
+    image_name = [image_name stringByAppendingString:@"-active"];
+  return [[UIImage imageNamed:image_name]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+}
+
+@end
