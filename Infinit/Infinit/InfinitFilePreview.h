99feca79459448e2f44045378c5a8b2931411e7b
Christopher Crone <christopher.crone@infinit.io>
2015-04-17 12:22:20 +0200

    FilePreview: Use bits to represent file type.

--- Infinit/Infinit/InfinitFilePreview.h
+++ Infinit/Infinit/InfinitFilePreview.h
@@ -11,19 +11,20 @@
 
 typedef NS_ENUM(NSUInteger, InfinitFileTypes)
 {
-  InfinitFileTypeArchive,
-  InfinitFileTypeAudio,
-  InfinitFileTypeDirectory,
-  InfinitFileTypeDocument,
-  InfinitFileTypeIllustrator,
-  InfinitFileTypeImage,
-  InfinitFileTypePhotoshop,
-  InfinitFileTypePresentation,
-  InfinitFileTypeSketch,
-  InfinitFileTypeSpreadsheet,
-  InfinitFileTypeVideo,
+  InfinitFileTypeArchive      = 1 << 0,
+  InfinitFileTypeAudio        = 1 << 1,
+  InfinitFileTypeDirectory    = 1 << 2,
+  InfinitFileTypeDocument     = 1 << 3,
+  InfinitFileTypeIllustrator  = 1 << 4,
+  InfinitFileTypeImage        = 1 << 5,
+  InfinitFileTypePhotoshop    = 1 << 6,
+  InfinitFileTypePresentation = 1 << 7,
+  InfinitFileTypeSketch       = 1 << 8,
+  InfinitFileTypeSpreadsheet  = 1 << 9,
+  InfinitFileTypeVideo        = 1 << 10,
 
-  InfinitFileTypeOther,
+  InfinitFileTypeOther        = 1 << 11,
+  InfinitFileTypeAll          = NSUIntegerMax,
 };
 
 @interface InfinitFilePreview : NSObject
