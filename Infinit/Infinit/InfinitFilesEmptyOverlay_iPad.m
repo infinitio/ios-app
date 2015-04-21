2249ee64af640e365a22d7ff07ea5f439392ae91
Christopher Crone <christopher.crone@infinit.io>
2015-04-21 19:07:24 +0200

    FilesEmptyOverlay.iPad: Add overlay for when there are no files.

---
+++ Infinit/Infinit/InfinitFilesEmptyOverlay_iPad.m
@@ -0,0 +1,13 @@
+//
+//  InfinitFilesEmptyOverlay_iPad.m
+//  Infinit
+//
+//  Created by Christopher Crone on 21/04/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitFilesEmptyOverlay_iPad.h"
+
+@implementation InfinitFilesEmptyOverlay_iPad
+
+@end
