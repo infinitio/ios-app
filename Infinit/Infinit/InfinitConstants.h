87f9dfce566ce5c263a139ad7ebe0cc44d359ae4
Christopher Crone <christopher.crone@infinit.io>
2015-04-28 14:32:46 +0200

    Constants: Add adjust token.

--- Infinit/Infinit/InfinitConstants.h
+++ Infinit/Infinit/InfinitConstants.h
@@ -9,21 +9,24 @@
 #ifndef Infinit_InfinitConstants_h
 #define Infinit_InfinitConstants_h
 
-/// URLs used for rating from the application.
-#define kInfinitStoreRatingLinkiOS8 @"https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software"
-#define kInfinitStoreRatingLink @"https://itunes.apple.com/app/apple-store/idAPP_ID"
+/// Adjust.io token.
+#define kInfinitAdjustToken @"4uuen7zlcxak"
 
 /// App Store ID.
 #define kInfinitAppStoreId @"955849852"
-
-/// Photos gallery name.
-#define kInfinitAlbumName @"Infinit"
 
 /// Application group name.
 #define kInfinitAppGroupName @"group.io.infinit.InfinitMobile"
 
 /// Forgot password URL.
 #define kInfinitForgotPasswordURL @"https://infinit.io/forgot_password?utm_source=app&utm_medium=ios&utm_campaign=forgot_password"
+
+/// Photos gallery name.
+#define kInfinitAlbumName @"Infinit"
+
+/// Ratings URLs.
+#define kInfinitStoreRatingLinkiOS8 @"https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software"
+#define kInfinitStoreRatingLink @"https://itunes.apple.com/app/apple-store/idAPP_ID"
 
 /// Web profile URL.
 #define kInfinitWebProfileURL @"https://infinit.io/account?utm_source=app&utm_medium=ios&utm_campaign=web_profile"
