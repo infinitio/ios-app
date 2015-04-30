256a602ce977838069b162a8d3c91d5effb91c41
Christopher Crone <christopher.crone@infinit.io>
2015-04-30 16:05:33 +0200

    WelcomeNavigationController: Initial fix for portrait only.

---
+++ Infinit/Infinit/InfinitWelcomeNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  InfinitWelcomeNavigationController.h
+//  Infinit
+//
+//  Created by Christopher Crone on 30/04/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitPortraitNavigationController.h"
+
+@interface InfinitWelcomeNavigationController : InfinitPortraitNavigationController
+
+@end
---
+++ Infinit/Infinit/InfinitWelcomeNavigationController.m
@@ -0,0 +1,32 @@
+//
+//  InfinitWelcomeNavigationController.m
+//  Infinit
+//
+//  Created by Christopher Crone on 30/04/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitWelcomeNavigationController.h"
+
+@interface InfinitWelcomeNavigationController ()
+
+@end
+
+@implementation InfinitWelcomeNavigationController
+
+- (BOOL)shouldAutorotate
+{
+  return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+  return UIInterfaceOrientationMaskPortrait;
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+  return UIInterfaceOrientationPortrait;
+}
+
+@end
