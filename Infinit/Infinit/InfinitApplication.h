b1e8792c361be23b4d22ecc44ad133057ba8662c
Christopher Crone <christopher.crone@infinit.io>
2015-06-11 17:52:54 +0200

    Application: Add application subclass to catch shake events.

---
+++ Infinit/Infinit/InfinitApplication.h
@@ -0,0 +1,13 @@
+//
+//  InfinitApplication.h
+//  Infinit
+//
+//  Created by Christopher Crone on 04/06/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinitApplication : UIApplication
+
+@end
---
+++ Infinit/Infinit/InfinitApplication.m
@@ -0,0 +1,29 @@
+//
+//  InfinitApplication.m
+//  Infinit
+//
+//  Created by Christopher Crone on 04/06/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "InfinitApplication.h"
+
+#import "AppDelegate.h"
+
+@implementation InfinitApplication
+
+- (void)sendEvent:(UIEvent*)event
+{
+  if (event && (event.subtype==UIEventSubtypeMotionShake))
+  {
+    AppDelegate* delegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+    [delegate handleShakeEvent:event];
+    [super sendEvent:event];
+  }
+  else
+  {
+    [super sendEvent:event];
+  }
+}
+
+@end
