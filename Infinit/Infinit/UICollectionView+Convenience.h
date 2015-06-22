ca70229858c73e720d022c334f831198d73d1618
Christopher Crone <christopher.crone@infinit.io>
2015-06-22 17:53:17 +0200

    UICollectionViewConvenience: Refactor.

---
+++ Infinit/Infinit/UICollectionView+Convenience.h
@@ -0,0 +1,15 @@
+//
+//  UICollectionView+Convenience.h
+//  Infinit
+//
+//  Created by Christopher Crone on 18/06/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICollectionView (Convenience)
+
+- (NSArray*)infinit_indexPathsForElementsInRect:(CGRect)rect;
+
+@end
---
+++ Infinit/Infinit/UICollectionView+Convenience.m
@@ -0,0 +1,27 @@
+//
+//  UICollectionView+Convenience.m
+//  Infinit
+//
+//  Created by Christopher Crone on 18/06/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import "UICollectionView+Convenience.h"
+
+@implementation UICollectionView (Convenience)
+
+- (NSArray*)infinit_indexPathsForElementsInRect:(CGRect)rect
+{
+  NSArray* all_layout_attrs = [self.collectionViewLayout layoutAttributesForElementsInRect:rect];
+  if (all_layout_attrs.count == 0)
+    return nil;
+  NSMutableArray* indexes = [NSMutableArray arrayWithCapacity:all_layout_attrs.count];
+  for (UICollectionViewLayoutAttributes* attrs in all_layout_attrs)
+  {
+    NSIndexPath* index = attrs.indexPath;
+    [indexes addObject:index];
+  }
+  return indexes;
+}
+
+@end
