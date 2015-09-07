deb9baa7041c3d0534793016dce97ec1e2f119f9
Christopher Crone <christopher.crone@infinit.io>
2015-09-07 16:03:39 +0200

    DeviceIdManager: Add manager for storing device id in the keychain so that it persists between installs.

---
+++ Infinit/Infinit/InfinitDeviceIdManager.h
@@ -0,0 +1,15 @@
+//
+//  InfinitDeviceIdManager.h
+//  Infinit
+//
+//  Created by Christopher Crone on 07/09/15.
+//  Copyright (c) 2015 Infinit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InfinitDeviceIdManager : NSObject
+
++ (void)checkExistingOrStoreCurrentDeviceId;
+
+@end
