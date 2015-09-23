1b60b370984b6fe036b01ff0aed6eb8204d3aaca
Christopher Crone <christopher.crone@infinit.io>
2015-09-23 09:10:52 +0200

    NonLocalizedString: Add wrapper so that non-localized strings that're user facing can be found easily.

---
+++ Infinit/Infinit/InfinitNonLocalizedString.h
@@ -0,0 +1,16 @@
+//
+//  InfinitNonLocalizedString.h
+//  Infinit
+//
+//  Created by Chris Crone on 17/09/15.
+//  Copyright © 2015 Infinit. All rights reserved.
+//
+
+#ifndef InfinitNonLocalizedString_h
+#define InfinitNonLocalizedString_h
+
+#define InfinitNonLocalizedString(string) \
+string
+
+
+#endif /* InfinitNonLocalizedString_h */
