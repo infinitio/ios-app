b9abab363e5802c93e6870fc20d5cbb80ec8c9a3
Christopher Crone <christopher.crone@infinit.io>
2015-09-29 12:35:30 +0200

    MessagingManager: Handle native emails.

--- Infinit/Infinit/InfinitMessagingManager.h
+++ Infinit/Infinit/InfinitMessagingManager.h
@@ -36,4 +36,19 @@ typedef void(^InfinitSendMessageCompletionBlock)(InfinitMessagingRecipient* reci
         toRecipient:(InfinitMessagingRecipient*)recipient
     completionBlock:(InfinitSendMessageCompletionBlock)completion_block;
 
+/** Show a prefilled messaging window for a contact so that the user can send the message.
+ @param message
+  Message that should be prefilled.
+ @param subject
+  Prefilled message subject. Currently only used for native emails.
+ @param recipient
+  Recipient that should receive the message.
+ @param completion_block
+  Block to be run on completion.
+ */
+- (void)sendMessage:(NSString*)message
+        withSubject:(NSString*)subject
+        toRecipient:(InfinitMessagingRecipient*)recipient
+    completionBlock:(InfinitSendMessageCompletionBlock)completion_block;
+
 @end
--- Infinit/Infinit/InfinitMessagingManager.mm
+++ Infinit/Infinit/InfinitMessagingManager.mm
@@ -14,19 +14,22 @@
 #import "InfinitMessagingRecipient.h"
 
 #import <MessageUI/MessageUI.h>
+#import <Gap/NSString+URLEncode.h>
 
 #undef check
 #import <elle/log.hh>
 
 ELLE_LOG_COMPONENT("iOS.MessagingManager");
 
-@interface InfinitMessagingManager () <MFMessageComposeViewControllerDelegate>
+@interface InfinitMessagingManager () <MFMailComposeViewControllerDelegate,
+                                       MFMessageComposeViewControllerDelegate>
 
 @property (atomic, readwrite) InfinitMessageStatus last_status;
 @property (nonatomic, readonly) dispatch_queue_t queue;
 @property (nonatomic, readonly) UIViewController* main_view_controller;
 @property (atomic, readwrite) dispatch_semaphore_t message_sent;
-@property (atomic, readwrite) MFMessageComposeViewController* sms_controller;
+@property (atomic, readwrite) MFMailComposeViewController* native_email_controller;
+@property (atomic, readwrite) MFMessageComposeViewController* native_message_controller;
 
 @end
 
@@ -67,6 +70,14 @@ static dispatch_once_t _instance_token = 0;
         toRecipient:(InfinitMessagingRecipient*)recipient
     completionBlock:(InfinitSendMessageCompletionBlock)completion_block
 {
+  [self sendMessage:message withSubject:nil toRecipient:recipient completionBlock:completion_block];
+}
+
+- (void)sendMessage:(NSString*)message
+        withSubject:(NSString*)subject
+        toRecipient:(InfinitMessagingRecipient*)recipient
+    completionBlock:(InfinitSendMessageCompletionBlock)completion_block
+{
   __weak InfinitMessagingManager* weak_self = self;
   dispatch_async(self.queue, ^
   {
@@ -74,18 +85,40 @@ static dispatch_once_t _instance_token = 0;
       return;
     InfinitMessagingManager* strong_self = weak_self;
     strong_self.message_sent = dispatch_semaphore_create(0);
-    if (recipient.method == InfinitMessageEmail)
+    if (recipient.method == InfinitMessageMetaEmail)
     {
       self.last_status = InfinitMessageStatusSuccess;
       dispatch_semaphore_signal(strong_self.message_sent);
     }
-    else if (recipient.method == InfinitMessageNative)
+    else if (recipient.method == InfinitMessageNativeEmail)
     {
-      strong_self.sms_controller = [[MFMessageComposeViewController alloc] init];
-      strong_self.sms_controller.recipients = @[recipient.identifier];
-      strong_self.sms_controller.body = message;
-      strong_self.sms_controller.messageComposeDelegate = strong_self;
-      [strong_self.main_view_controller presentViewController:strong_self.sms_controller
+      if ([MFMailComposeViewController canSendMail])
+      {
+        strong_self.native_email_controller = [[MFMailComposeViewController alloc] init];
+        strong_self.native_email_controller.toRecipients = @[recipient.identifier];
+        [strong_self.native_email_controller setMessageBody:message isHTML:NO];
+        strong_self.native_email_controller.subject = subject;
+        strong_self.native_email_controller.mailComposeDelegate = self;
+        [strong_self.main_view_controller presentViewController:strong_self.native_email_controller
+                                                       animated:YES
+                                                     completion:NULL];
+      }
+      else
+      {
+        NSString* mail_to = [NSString stringWithFormat:@"mailto:%@?Subject=%@&Body=%@",
+                             recipient.identifier.infinit_URLEncoded,
+                             subject.infinit_URLEncoded,
+                             message.infinit_URLEncoded];
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:mail_to]];
+      }
+    }
+    else if (recipient.method == InfinitMessageNativeSMS)
+    {
+      strong_self.native_message_controller = [[MFMessageComposeViewController alloc] init];
+      strong_self.native_message_controller.recipients = @[recipient.identifier];
+      strong_self.native_message_controller.body = message;
+      strong_self.native_message_controller.messageComposeDelegate = strong_self;
+      [strong_self.main_view_controller presentViewController:strong_self.native_message_controller
                                                      animated:YES
                                                    completion:NULL];
     }
@@ -101,17 +134,22 @@ static dispatch_once_t _instance_token = 0;
                            contactIdentifier:recipient.address_book_id];
       }
     }
+    else
+    {
+      ELLE_WARN("%s: unhandled message type (%d): %s",
+                self.description.UTF8String, recipient.method, recipient.method_description);
+      dispatch_semaphore_signal(strong_self.message_sent);
+    }
     dispatch_semaphore_wait(strong_self.message_sent, DISPATCH_TIME_FOREVER);
     completion_block(recipient, message, strong_self.last_status);
   });
 }
 
-- (void)_openWhatsAppForMessage:(NSString*)message_
+- (void)_openWhatsAppForMessage:(NSString*)message
               contactIdentifier:(int32_t)identifier
 {
-  NSString* message = [message_ stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-  NSString* url_str =
-    [NSString stringWithFormat:@"whatsapp://send?abid=%d&text=%@", identifier, message];
+  NSString* url_str = [NSString stringWithFormat:@"whatsapp://send?abid=%d&text=%@",
+                       identifier, message.infinit_URLEncoded];
   NSURL* url = [NSURL URLWithString:url_str];
   [[UIApplication sharedApplication] openURL:url];
 }
@@ -125,9 +163,40 @@ static dispatch_once_t _instance_token = 0;
   dispatch_semaphore_signal(self.message_sent);
 }
 
+#pragma mark - MFMailComposeViewControllerDelegate
+
+- (void)mailComposeController:(MFMailComposeViewController*)controller 
+          didFinishWithResult:(MFMailComposeResult)result
+                        error:(nullable NSError*)error
+{
+  switch (result)
+  {
+    case MFMailComposeResultCancelled:
+    case MFMailComposeResultSaved:
+      self.last_status = InfinitMessageStatusCancel;
+      break;
+    case MFMailComposeResultFailed:
+      self.last_status = InfinitMessageStatusFail;
+      break;
+    case MFMailComposeResultSent:
+      self.last_status = InfinitMessageStatusSuccess;
+      break;
+      
+    default:
+      break;
+  }
+  [self.native_email_controller dismissViewControllerAnimated:YES
+                                                     completion:^
+   {
+     dispatch_semaphore_signal(self.message_sent);
+     self.native_email_controller = nil;
+   }];
+}
+
 #pragma mark - MFMessageComposeViewControllerDelegate
 
-- (void)_handleResult:(MessageComposeResult)result
+- (void)messageComposeViewController:(MFMessageComposeViewController*)controller
+                 didFinishWithResult:(MessageComposeResult)result
 {
   switch (result)
   {
@@ -140,22 +209,16 @@ static dispatch_once_t _instance_token = 0;
     case MessageComposeResultSent:
       self.last_status = InfinitMessageStatusSuccess;
       break;
-
+      
     default:
       break;
   }
-  [self.sms_controller dismissViewControllerAnimated:YES
-                                          completion:^
-  {
-    dispatch_semaphore_signal(self.message_sent);
-    self.sms_controller = nil;
-  }];
-}
-
-- (void)messageComposeViewController:(MFMessageComposeViewController*)controller
-                 didFinishWithResult:(MessageComposeResult)result
-{
-  [self _handleResult:result];
+  [self.native_message_controller dismissViewControllerAnimated:YES
+                                                     completion:^
+   {
+     dispatch_semaphore_signal(self.message_sent);
+     self.native_message_controller = nil;
+   }];
 }
 
 #pragma mark - Helpers
